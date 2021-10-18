b12057d5bf80ac5e5325479fe8a0989edb47cba1
Your Name <you@example.com>
2021-10-18 12:52:45 +0200

    Dodano zadania i skompilowane pliki exe

---
+++ zad1.cpp
@@ -0,0 +1,29 @@
+//Napisz program który bedzie sprawdzał czy wprowadzona liczba jest podzielna przez 3 i 5, 
+//jezeli bedzie podzielna to oblicz jej pierwiastek
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    
+    cout<<"Program obliczajacy pierwiastek liczb podzielnych przez 3 i 5\n\n";
+
+    //definicja zmiennej przechowujacej liczby calkowite a
+    int a;
+
+    //zebranie wartosci od uzytkownika
+    cout<<"Podaj liczbe: ";
+    cin>>a;
+
+    //sprawdzenie czy liczba jest podzielna przez 3 i 5
+    if(a%3 == 0 && a%5 == 0){
+        //zwrocenie pierwiastka liczby jesli jest podzielna
+        cout<<"Pierwiastek tej liczby to "<<(double)(sqrt(a));
+    }else{
+        //odmowa obliczenia pierwiastka gdy liczba nie spelnia warunku
+        cout<<"Liczba nie spelnia warunkow, nie obliczam pierwiastka";
+    }
+
+    return 0;
+}
---
+++ zad2.cpp
@@ -0,0 +1,25 @@
+//Napisz program wyznaczajacy najmniejsza z trzech liczb podanych przez uzytkownika
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+
+    cout<<"Program znajdujacy najmniejsza podana wartosc\n\n";
+
+    //definicja trzyelementowej tablicy liczb calkowitych
+    int arr[3];
+
+    //zebranie wartosci od uzytkownika
+    cout<<"Podaj trzy wartosci: \n";
+    cin>>arr[0]>>arr[1]>>arr[2];
+    
+    //posortowanie tabeli w kolejnosci rosnacej
+    sort(arr,arr+3);
+
+    //zwrocenie pierwszej (po posortowaniu najmniejszej) liczby z podanych
+    cout<<"Najmniejsza wartosc to "<<arr[0];
+
+    return 0;
+}
